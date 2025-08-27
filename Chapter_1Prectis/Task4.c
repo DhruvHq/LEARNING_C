7f0156ae9295a25499c408acc7462ca2fe5d0332
Dhruv <dhruvpatel24a@gmail.com>
2025-08-27 21:31:06 +0530

    Update Task4.c

--- Chapter_1Prectis/Task4.c
+++ Chapter_1Prectis/Task4.c
@@ -1,4 +1,4 @@
-/*Fun Mini-Project
+/*Mini-Project
 
 Ask the user for:
 Name
@@ -6,3 +6,25 @@ Age
 Favorite number
 Print a message:
 Hello Name! At Age, your favorite number is X.*/
+
+#include<stdio.h>
+int main(){
+
+    char Name [50];
+    int Age;
+    int Number;
+
+    printf("Give Me Asked Details :\n\n");
+
+    printf("Whats Your Name Pal?\n");
+    scanf("%s", Name);
+    printf("How Old Are You Pal?\n");
+    scanf("%d",&Age);
+    printf("If You Are Kind Enough To Share Your Favorit Number :\n\n");
+    scanf("%d",&Number);
+
+    printf("Hello %s You Are %d Years Old And Your Favorit Number Is %d",Name, Age, Number);
+
+    return 0;
+
+}
